a2ec854e4a3914b9a3bd7b7bd4be9d300cfc5097
Захаров Павел <pzakharov83@gmail.com>
2025-03-19 16:03:42 +0300

    bu

--- core/src/main/java/ru/yandexpraktikum/core/presentation/mappers/PresentationNoteMapper.kt
+++ core/src/main/java/ru/yandexpraktikum/core/presentation/mappers/PresentationNoteMapper.kt
@@ -2,21 +2,18 @@ package ru.yandexpraktikum.core.presentation.mappers
 
 import ru.yandexpraktikum.core.domain.model.Note
 import ru.yandexpraktikum.core.presentation.model.NoteUi
+import javax.inject.Inject
 
-class PresentationNoteMapper {
+class PresentationNoteMapper @Inject constructor() {
     fun mapToUi(note: Note): NoteUi {
         return NoteUi(
-            id = note.id,
-            title = note.title,
-            content = note.content
+            id = note.id, title = note.title, content = note.content
         )
     }
 
     fun mapToDomain(note: NoteUi): Note {
         return Note(
-            id = note.id,
-            title = note.title,
-            content = note.content
+            id = note.id, title = note.title, content = note.content
         )
     }
 }
