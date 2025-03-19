5912622d3dab7e4e3f9e0f5645c3165e763f1a87
Захаров Павел <pzakharov83@gmail.com>
2025-03-19 16:56:38 +0300

    add all notes sub

---
+++ all-notes/src/main/java/ru/yandexpraktikum/all_notes/di/AllNotesModule.kt
@@ -0,0 +1,20 @@
+package ru.yandexpraktikum.all_notes.di
+
+import dagger.Binds
+import dagger.Module
+import ru.yandexpraktikum.all_notes.domain.interactors.DeleteNoteInteractor
+import ru.yandexpraktikum.all_notes.domain.interactors.DeleteNoteInteractorImpl
+import ru.yandexpraktikum.all_notes.domain.interactors.FetchAllNotesInteractor
+import ru.yandexpraktikum.all_notes.domain.interactors.FetchAllNotesInteractorImpl
+
+@Module
+interface AllNotesModule {
+
+    @AllNotesScope
+    @Binds
+    fun provideFetchAllNotesInteractor(impl: FetchAllNotesInteractorImpl): FetchAllNotesInteractor
+
+    @AllNotesScope
+    @Binds
+    fun provideDeleteNoteInteractor(impl: DeleteNoteInteractorImpl): DeleteNoteInteractor
+}
--- all-notes/src/main/java/ru/yandexpraktikum/all_notes/presentation/AllNotesViewModelFactory.kt
+++ all-notes/src/main/java/ru/yandexpraktikum/all_notes/presentation/AllNotesViewModelFactory.kt
@@ -5,16 +5,20 @@ import androidx.lifecycle.ViewModelProvider
 import ru.yandexpraktikum.all_notes.domain.interactors.DeleteNoteInteractor
 import ru.yandexpraktikum.all_notes.domain.interactors.FetchAllNotesInteractor
 import ru.yandexpraktikum.core.presentation.mappers.PresentationNoteMapper
+import javax.inject.Inject
 
-class AllNotesViewModelFactory(
+class AllNotesViewModelFactory @Inject constructor(
     private val fetchAllNotesInteractor: FetchAllNotesInteractor,
     private val deleteNoteInteractor: DeleteNoteInteractor,
-    private val noteMapper: PresentationNoteMapper
+    private val noteMapper: PresentationNoteMapper,
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(AllNotesViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return AllNotesViewModel(fetchAllNotesInteractor, deleteNoteInteractor, noteMapper) as T
+            @Suppress("UNCHECKED_CAST") return AllNotesViewModel(
+                fetchAllNotesInteractor,
+                deleteNoteInteractor,
+                noteMapper
+            ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
